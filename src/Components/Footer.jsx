af9ad50f83878c972f96323c5bc80551bc4b9db7
galib006 <galibmaruf@gmail.com>
2025-10-13 18:34:46 +0600

    Update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,17 +5,22 @@ import { FaFacebookF } from "react-icons/fa";
 import logo from "../assets/logo.png";
 function Footer() {
   return (
-    <div>
-      <div className="flex justify-between px-20 bg-[#001931]">
-        <div className="gap-1.5 flex items-center text-base font-bold  text-white"><img src={logo} alt="Logo" className="w-10" /> HERO.IO</div>
-        <div>
-          <h4 className="text-white text-xl space-x-2">Social Links</h4>
-          <ul className="flex text-white">
-            <li><a href="#" className="rounded-full p-1.5 bg-white"><FaXTwitter /></a></li>
-            <li><a href="#" className="rounded-full p-1.5 bg-white"><FaLinkedinIn /></a></li>
-            <li><a href="#" className="rounded-full p-1.5 bg-white"><FaFacebookF /></a></li>
+    <div className="bg-[#001931] pt-9 ">
+      <div className="px-20 ">
+        <div className="border-b-1 border-b-[#ffffff42] flex justify-between ">
+          <div className="gap-1.5 flex items-center text-base font-bold  text-white"><img src={logo} alt="Logo" className="w-10" /> HERO.IO</div>
+        <div className="">
+          <h4 className="text-white text-xl mb-3">Social Links</h4>
+          <ul className="flex text-black space-x-3">
+            <li><a href="#" className="block rounded-full p-1.5 bg-white"><FaXTwitter /></a></li>
+            <li><a href="#" className="block rounded-full p-1.5 bg-white"><FaLinkedinIn /></a></li>
+            <li><a href="#" className="block rounded-full p-1.5 bg-white"><FaFacebookF /></a></li>
           </ul>
         </div>
+        </div>
+      </div>
+      <div className="py-7">
+        <p className="text-[#FAFAFA] text-center text-base">Copyright © 2025 - All right reserved</p>
       </div>
     </div>
   );
