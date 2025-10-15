0cd492795be40a8026105e0cb0c1adecf286d098
galib006 <galibmaruf@gmail.com>
2025-10-15 13:32:52 +0600

    Fix scroll Top

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import ScrollToTop from "./Components/ScrollToTop";
+
 
 function App() {
   return (
     <>
+    <ScrollToTop></ScrollToTop>
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
